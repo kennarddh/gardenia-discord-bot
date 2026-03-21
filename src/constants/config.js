b538228a36d697dc6864d63a9a9fd36ccd33f496
kennarddh <79390992+kennarddh@users.noreply.github.com>
2026-03-21 23:29:07 +0700

    feat: Ability to start mutation

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,18 +1,18 @@
 module.exports = {
-    startingBloomBuck: 50,
-    baseGardenSlots: 5,
-    upgradeCost: 500,
+	startingBloomBuck: 50,
+	baseGardenSlots: 5,
+	upgradeCost: 500,
 
-    variants: [
-        { name: 'Normal', weight: 600, multiplier: 1 },
-        { name: 'Gold', weight: 100, multiplier: 2 },
-        { name: 'Shiny', weight: 50, multiplier: 5 },
-        { name: 'Withered', weight: 90, multiplier: 0.5 },
-        { name: 'Dead', weight: 10, multiplier: 0 }
-    ],
+	variants: [
+		{ name: 'Normal', weight: 600, multiplier: 1 },
+		{ name: 'Gold', weight: 100, multiplier: 2 },
+		{ name: 'Shiny', weight: 50, multiplier: 5 },
+		{ name: 'Withered', weight: 90, multiplier: 0.5 },
+		{ name: 'Dead', weight: 10, multiplier: 0 },
+	],
 
-    mutations: [
-        { name: 'Wet', chance: 0.9, multiplier: 1.5 },
-        { name: 'Shocked', chance: 0.3, multiplier: 5 },
-    ],
-};
+	mutations: [
+		{ name: 'Wet', affectedChance: 0.9, occuranceWeight: 200, multiplier: 1.5 },
+		{ name: 'Shocked', affectedChance: 0.3, occuranceWeight: 100, multiplier: 5 },
+	],
+}
